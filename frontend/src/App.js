aab89ceb877edff252e2d1234027830de364a2a2
Muheto Niyonemera <muheto.niyonemera@gmail.com>
2025-10-03 03:55:57 +0100

    Added a README.md file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,17 +29,6 @@ function App() {
     setIsAuthenticated(false);
   };
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </React.StrictMode>
-  );
-
-  reportWebVitals();
-
   // Main App Layout for authenticated users
   const AppLayout = ({ children }) => (
     <div className="App">
