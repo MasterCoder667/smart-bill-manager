cdac957c30ba88483d16db86abee19dc7ac6231b
Muheto Niyonemera <ZNAC504@live.rhul.ac.uk>
2025-10-04 02:17:25 +0100

    Bug fixing

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./subscriptions.db" # Creates basic database
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./subscriptions.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args = {"check_same_thread" : False}
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
@@ -21,4 +19,3 @@ def get_db():
         yield db
     finally:
         db.close()
-        
