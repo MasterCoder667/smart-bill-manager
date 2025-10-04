4b0eff64d4ab4fea0874a63d7f133e9d02c78850
Muheto Niyonemera <ZNAC504@live.rhul.ac.uk>
2025-10-04 02:13:32 +0100

    Fixing 502 bad gateway error

--- backend/main.py
+++ backend/main.py
@@ -256,10 +256,4 @@ def delete_subscription(
     return {"message": "Subscription deleted successfully"}
 
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
-
-
 
