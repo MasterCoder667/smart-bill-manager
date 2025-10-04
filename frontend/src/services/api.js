f86398430a4c479168a5c4aebbce5df2b2fb514b
Muheto Niyonemera <ZNAC504@live.rhul.ac.uk>
2025-10-04 01:49:20 +0100

    Fixes of bugs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = "https://backend-service-production-b411.up.railway.app";
 
 // Create axios instance with base configuration
 const api = axios.create({
